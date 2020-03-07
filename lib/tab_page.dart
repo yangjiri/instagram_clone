d890822d3741abfb6948a17ebf697caa44dd9ffe
yangjiri <tnvjwldms1541@gmail.com>
2020-03-07 18:13:22 +0900

    screen construction2

---
+++ lib/tab_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text('top page'),
+    );
+  }
+}
